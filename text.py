3b29ae99bbbcb99b2856049618f88b709f438ba8
Lim Lee <poi890poi@gmail.com>
2020-04-25 16:33:37 +0800

    Add more functions and properties to splitted components

--- text.py
+++ text.py
@@ -30,37 +30,128 @@ def count_width_chars(text):
 
 
 class SplittedComponents(str):
+    '''
+    Split text by spaces. Return intervals and splitted components.
+    '''
 
-    __RE_EMPTY = re.compile('^[ ]*$')
-    __RE_SPACES_SPLITTING = re.compile('([ ]*)([^ ]+)([ ]*)')
+    RE_EMPTY = re.compile('^[ ]*$')
+    RE_SPACES_SPLITTING = re.compile('([ ]*)([^ ]+)([ ]*)')
+    RE_METRICS = re.compile('(\\d+)([^ \\d]+)')
+    RE_CODE = re.compile('([A-Z][A-Z\\d]+)')
+    RE_DATE = re.compile('(\\d+年)[^\\d]*(\\d+月)')
 
     def __new__(cls, text, *args, **kwargs):
         return super(SplittedComponents, cls).__new__(cls, text)
 
     def __init__(self, text, *args, **kwargs):
+        self.update_text(text)
+
+    def update_text(self, text):
         self.__text = text
-        components = self.__RE_SPACES_SPLITTING.findall(text)
+        components = self.RE_SPACES_SPLITTING.findall(text)
         components = np.array([c for c in chain.from_iterable(components) if c])
         self.__components = components
-        end = np.cumsum([len(c) for c in components])
-        begin = np.hstack(([0,], end[:-1])).reshape(-1, 1)
-        end = end.reshape(-1, 1)
-        self.__intervals = np.hstack((begin, end)).astype(np.uint)
-        self.__empty = np.array([True if self.__RE_EMPTY.match(c) else False for c in components], dtype=bool)
+        self.__intervals = self.splitters_to_interval([len(c) for c in components])
+        self.__empty = np.array([True if self.RE_EMPTY.match(c) else False for c in components], dtype=bool)
         print(self.__components, self.__intervals, self.__empty)
+
+    @classmethod
+    def __is_empty(cls, component):
+        # TODO A more sophiscated method to include full-wdith space is required
+        return component.strip()
+
+    @property
+    def text(self):
+        return self.__text
+
+    @property
+    def intervals(self):
+        return self.__intervals
+
+    @property
+    def components(self):
+        return self.__components
+
+    @property
+    def nonempty_splitters_right(self):
+        # Backward compatibility for legacy codes
+        splitters = [s for i, s in enumerate(self.splitters) if not self.__empty[i]]
+        return splitters
+
+    @property
+    def splitters(self):
+        # Backward compatibility for legacy codes
+        _, splitters = list(zip(*self.__intervals))
+        return splitters
+
+    @classmethod
+    def splitters_to_interval(cls, splitters):
+        end = np.cumsum(splitters)
+        begin = np.copy(end)
+        begin = np.roll(begin, 1)
+        begin[0] = 0
+        begin = begin.reshape(-1, 1)
+        end = end.reshape(-1, 1)
+        return np.hstack((begin, end)).astype(np.uint)
 
     @property
     def nonempty_components(self):
         components = [c for i, c in enumerate(self.__components) if not self.__empty[i]]
         return components
 
+    @property
+    def stats(self):
+        # Find number of numeric components in the line
+        n_nonempty = 0
+        n_numeric = 0
+        n_metric = 0
+        n_code = 0
+        n_date = 0
+        for c in self.__components:
+            if self.__is_empty(c):
+                n_nonempty += 1
+            try:
+                float(c)
+                n_numeric += 1
+            except ValueError:
+                m = self.RE_METRICS.match(c)
+                if m:
+                    n_metric += 1
+                m = self.RE_CODE.match(c)
+                if m:
+                    n_code += 1
+                m = self.RE_DATE.match(c)
+                if m:
+                    n_date += 1
+        return n_numeric, n_metric, n_code, n_date, n_nonempty
 
-def splitted_components(text):
-    '''
-    Split text by spaces. Return intervals and splitted components.
-    '''
-    pass
+    def pad_spaces(self):
+        '''
+        Align half-width and full-width chars by adding spaces to the left 
+        of components. Assume that full-width is twice the width of half-width
+        chars.
+        '''
+        #residue = 0. # TODO Handle width ratio that can't be divided
+        if self.__text:
+            full_chars = count_full_chars(self.__text)
+            for i, c in enumerate(self.__components):
+                if not c.strip():
+                    spaces = 0
+                    spaces = len(c)
+                    try:
+                        full_chars = count_full_chars(self.__components[i+1])
+                        padding = full_chars
+                        spaces += padding
+                    except IndexError:
+                        pass
+                    self.__components[i] = ' ' * spaces
+            self.update_text(''.join(self.__components))
+
 
 if __name__ == '__main__':
-    line = SplittedComponents('       This is a pen.  ')
-    print(line.nonempty_components)
+    scomponents = SplittedComponents('       This is a pen.  ')
+    print(scomponents.components)
+    print(scomponents.intervals)
+    print(scomponents.nonempty_components)
+    print(scomponents.splitters)
+    print(scomponents.nonempty_splitters_right)
