c45e86f03789c0a8b0417a0a1db37d8858c343b8
Lim Lee <poi890poi@gmail.com>
2020-04-25 18:16:18 +0800

    Fix a bug in counting half/full width chars

--- text.py
+++ text.py
@@ -23,7 +23,7 @@ def count_width_chars(text):
     half_width = 0
     full_width = 0
     for c in text:
-        if not (0 <= ord(c) <= 127):
+        if 0 <= ord(c) <= 127:
             half_width += 1
         else:
             full_width += 1
