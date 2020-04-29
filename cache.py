4f166f13ec9de3984dada274b9ec9c70c5224373
Lim Lee <poi890poi@gmail.com>
2020-04-29 21:47:18 +0800

    Improved memory cache with bytes limit

--- cache.py
+++ cache.py
@@ -2,7 +2,7 @@ import pickle
 import json
 import time
 import hashlib
-import uuid
+import warnings
 from collections import deque, OrderedDict
 
 
@@ -11,31 +11,46 @@ __VERSION__ = '0.10'
 
 class MemCache():
 
-    MAX_LENGTH = 16
+    __MAX_LENGTH = 1024
+    __MAX_BYTES = 8 * 1024 * 1024
+
 
     @classmethod
     def __get_container(cls):
-        cls._EMPTY = uuid.uuid1()
         try:
             cls.__container
         except AttributeError:
             cls.__container = dict()
             cls.__queue = deque()
+            cls.__bytes = 0
         return cls.__container, cls.__queue
 
     @classmethod
     def _set(cls, key, data):
         c, dq = cls.__get_container()
-        dq.append(key)
-        while len(dq) > cls.MAX_LENGTH:
-            k_ = dq.popleft()
-            del c[k_]
+
+        pkl_str = pickle.dumps(data)
+        if cls.__MAX_BYTES and len(pkl_str) > cls.__MAX_BYTES:
+            warnings.warn('Object {} is too large to be cached in memory'.format(key), ResourceWarning)
 
         cache_obj = {
             '__timestamp': time.time(),
-            'data': pickle.dumps(data),
+            'data': pkl_str,
         }
         c[key] = cache_obj
+        dq.append(key)
+        cls.__bytes += len(pkl_str)
+
+        if cls.__MAX_LENGTH:
+            while len(dq) > cls.__MAX_LENGTH:
+                k_ = dq.popleft()
+                cls.__bytes -= len(c[k_]['data'])
+                del c[k_]
+        if cls.__MAX_BYTES:
+            while cls.__bytes > cls.__MAX_BYTES:
+                k_ = dq.popleft()
+                cls.__bytes -= len(c[k_]['data'])
+                del c[k_]
     
     @classmethod
     def _get(cls, key):
@@ -79,7 +94,7 @@ def mem_cached(*args, **kwargs):
 
 @mem_cached()
 def test(n):
-    return n * n
+    return ([0] * n) * n
 
 @mem_cached(prefix='prefixed')
 def test2(n):
@@ -88,7 +103,13 @@ def test2(n):
 if __name__ == '__main__':
     print(test(9))
     print(test(8))
-    print(test(9))
-    print(test2(9))
+    print(len(test(9)))
+    print('test2', test2(9))
+    print(len(test(9)))
+    for i in range(900, 920):
+        print(i, len(test(i)))
+    for i in range(5):
+        print('908', len(test(918)))
+    print('900', len(test(9000)))
 
         
